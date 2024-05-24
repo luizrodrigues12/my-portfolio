34bcf3dd71767b572281075e7c002e9ba209d13c
luizrodrigues12 <luizrodrigues2021@icloud.com>
2024-05-24 13:24:43 -0300

    Requesting api

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import About from "./sections/About/About";
 import Hero from "./sections/Hero/Hero";
 import Projects from "./sections/Projects/Projects";
+import axios from "axios";
 
 const Home = () => {
+  useEffect(() => {
+    axios
+      .get("https://todo-luiz-back.onrender.com/get")
+      .then((res) => res)
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div
       style={{
